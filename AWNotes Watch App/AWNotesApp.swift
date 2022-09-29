e71a33c2e235ea6c898da05e61a64707e5e02a63
KevinhoMorales <kfmorales94@gmail.com>
2022-09-28 22:23:24 -0500

    Init project

---
+++ AWNotes Watch App/AWNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWNotesApp.swift
+//  AWNotes Watch App
+//
+//  Created by Kevin Morales on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct AWNotes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
